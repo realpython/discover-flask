084364d099d50bb0cb1902331c058c30e0100d52
Michael Herman <hermanmu@gmail.com>
2014-06-03 12:00:13 -0500

    added part1

---
+++ app.py
@@ -0,0 +1,18 @@
+# import the Flask class from the flask module
+from flask import Flask, render_template
+
+# create the application object
+app = Flask(__name__)
+
+# use decorators to link the function to a url
+@app.route('/')
+def home():
+    return "Hello, World!"  # return a string
+
+@app.route('/welcome')
+def welcome():
+    return render_template('welcome.html')  # render a template
+
+# start the server wuth the 'run()' method
+if __name__ == '__main__':
+    app.run(debug=True)
