866e76b9cc88293bc0695519a12551172a956a2b
Michael Herman <hermanmu@gmail.com>
2014-06-05 13:21:28 -0500

    added user login

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # import the Flask class from the flask module
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 # create the application object
 app = Flask(__name__)
@@ -12,6 +12,18 @@ def home():
 @app.route('/welcome')
 def welcome():
     return render_template('welcome.html')  # render a template
+
+# route for handling the login page logic
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+
 
 # start the server with the 'run()' method
 if __name__ == '__main__':
