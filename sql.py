a3e941b3e890c3dae9347ebbf63ae16c825b1c0f
Michael Herman <hermanmu@gmail.com>
2014-06-12 19:29:10 -0500

    part 5 - databases

--- sql.py
+++ sql.py
@@ -4,13 +4,13 @@
 import sqlite3
 
 # create a new database if the database doesn't already exist
-with sqlite3.connect("sample.db") as connection:
+with sqlite3.connect('sample.db') as connection:
 
     # get a cursor object used to execute SQL commands
     c = connection.cursor()
 
     # create the table
-    c.execute("CREATE TABLE posts(title TEXT, details TEXT)")
+    c.execute('CREATE TABLE posts(title TEXT, details TEXT)')
 
     # insert dummy data into the table
     c.execute('INSERT INTO posts VALUES("Good", "I\'m good.")')
