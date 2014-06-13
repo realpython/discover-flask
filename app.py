24a7bdba80014dcad43fa783d6c1c45b94b740e7
Michael Herman <hermanmu@gmail.com>
2014-06-13 17:54:06 -0500

    list comp refactor

--- app.py
+++ app.py
@@ -31,7 +31,13 @@ def home():
     # return "Hello, World!"  # return a string
     g.db = connect_db()
     cur = g.db.execute('select * from posts')
-    posts = [dict(title=row[0], description=row[1]) for row in cur.fetchall()]
+
+    posts = []
+    for row in cur.fetchall():
+        posts.append(dict(title=row[0], description=row[1]))
+
+    # posts = [dict(title=row[0], description=row[1]) for row in cur.fetchall()]
+
     g.db.close()
     return render_template('index.html', posts=posts)  # render a template
 
