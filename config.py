bfa9ae83e3337613ca2cab093bd41b18c3efdc0d
Michael Herman <hermanmu@gmail.com>
2014-07-12 22:26:54 -0500

    updated session key

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 # default config
 class BaseConfig(object):
     DEBUG = False
-    SECRET_KEY = 'my precious'
+    SECRET_KEY = '\xbf\xb0\x11\xb1\xcd\xf9\xba\x8bp\x0c`,\xa2\xd2\x9c1G\x8f4\x07\x11\xac\xfc\x8d'
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 
