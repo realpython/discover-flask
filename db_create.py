f35143312ec612ff990729ad2286ebe401328a00
Michael Herman <hermanmu@gmail.com>
2014-08-07 20:40:02 -0500

    updated postgres for tmw's video

--- db_create.py
+++ db_create.py
@@ -9,6 +9,7 @@ db.session.add(BlogPost("Good", "I\'m good."))
 db.session.add(BlogPost("Well", "I\'m well."))
 db.session.add(BlogPost("Excellent", "I\'m excellent."))
 db.session.add(BlogPost("Okay", "I\'m okay."))
+db.session.add(BlogPost("postgres", "we setup a local postgres instance"))
 
 # commit the changes
 db.session.commit()
