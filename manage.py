0c884c86accb90c3c76af56b9cb1d193d253a7a6
Michael Herman <hermanmu@gmail.com>
2014-08-14 05:12:46 -0500

    migrations

---
+++ manage.py
@@ -0,0 +1,14 @@
+from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
+import os
+
+from app import app, db
+
+app.config.from_object(os.environ['APP_SETTINGS'])
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
--- models.py
+++ models.py
@@ -1,5 +1,8 @@
 from app import db
 
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+
 
 class BlogPost(db.Model):
 
@@ -8,6 +11,7 @@ class BlogPost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     description = db.Column(db.String, nullable=False)
+    author_id = db.Column(db.Integer, ForeignKey('users.id'), nullable=False)
 
     def __init__(self, title, description):
         self.title = title
@@ -15,3 +19,22 @@ class BlogPost(db.Model):
 
     def __repr__(self):
         return '<title {}'.format(self.title)
+
+
+class User(db.Model):
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    posts = relationship("BlogPost", backref="author")
+
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
+
+    def __repr__(self):
+        return '<name {}'.format(self.name)
