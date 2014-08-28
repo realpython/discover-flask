73e7842ea3648f5ee74a998b590e7e3f3f854cce
Michael Herman <hermanmu@gmail.com>
2014-08-27 17:04:33 -0700

    virtualenv wrapper update

--- db_create.py
+++ db_create.py
@@ -1,15 +1,15 @@
 from app import db
-from models import BlogPost
+# from models import BlogPost
 
 # create the database and the db table
 db.create_all()
 
 # insert data
-db.session.add(BlogPost("Good", "I\'m good."))
-db.session.add(BlogPost("Well", "I\'m well."))
-db.session.add(BlogPost("Excellent", "I\'m excellent."))
-db.session.add(BlogPost("Okay", "I\'m okay."))
-db.session.add(BlogPost("postgres", "we setup a local postgres instance"))
+# db.session.add(BlogPost("Good", "I\'m good."))
+# db.session.add(BlogPost("Well", "I\'m well."))
+# db.session.add(BlogPost("Excellent", "I\'m excellent."))
+# db.session.add(BlogPost("Okay", "I\'m okay."))
+# db.session.add(BlogPost("postgres", "we setup a local postgres instance"))
 
 # commit the changes
 db.session.commit()
