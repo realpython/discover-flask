c3184950f5eca73a5087e6e9ff39cbe606c2d6fc
Grey Li <withlihui@gmail.com>
2017-04-25 17:35:54 +0800

    Delete unnecessary code in config file

--- config.py
+++ config.py
@@ -7,7 +7,6 @@ class BaseConfig(object):
     # shortened for readability
     SECRET_KEY = '\xbf\xb0\x11\xb1\xcd\xf9\xba\x8bp\x0c...'
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-    print SQLALCHEMY_DATABASE_URI
 
 
 class TestConfig(BaseConfig):
